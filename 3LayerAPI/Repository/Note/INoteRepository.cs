784153c6cd9698a9a2d5772989c023e75fd2fef2
RogerCodes <rogelioherreradev@gmail.com>
2024-04-12 00:15:05 -0600

    Defined and Implemented 'Notes' Interface

--- 3LayerAPI/Repository/Note/INoteRepository.cs
+++ 3LayerAPI/Repository/Note/INoteRepository.cs
@@ -2,6 +2,17 @@
 {
     public interface INoteRepository
     {
-
+        Task<bool> NoteExistAsync(string title);
+        Task<bool> NoteExistAsync(int id);
+        Task<bool> CreateNote(Models.Note note);
+        Task<Models.Note> GetNoteByTitleAsync(string title);
+        Task<Models.Note> GetNoteByIdAsync(int id);
+        Task<ICollection<Models.Note>> GetAllNotesAsync();
+        Task<ICollection<Models.Note>> GetNotesAsync();
+        Task<ICollection<Models.Note>> GetPrivateNotesAsync();
+        Task<ICollection<Models.Note>> GetDeletedNotesAsync();
+        Task<bool> UpdateNoteAsync(Models.Note note);
+        Task<bool> SoftDeleteNoteAsync(int id);
+        Task<bool> HardDeleteNoteAsync(Models.Note note);
     }
 }
--- 3LayerAPI/Repository/Note/NoteRepository.cs
+++ 3LayerAPI/Repository/Note/NoteRepository.cs
@@ -1,6 +1,66 @@
-﻿namespace _3LayerAPI.Repository.Note
+﻿
+namespace _3LayerAPI.Repository.Note
 {
     public class NoteRepository : INoteRepository
     {
+        public Task<bool> CreateNote(Models.Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Models.Note>> GetAllNotesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Models.Note>> GetDeletedNotesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Models.Note> GetNoteByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Models.Note> GetNoteByTitleAsync(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Models.Note>> GetNotesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Models.Note>> GetPrivateNotesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HardDeleteNoteAsync(Models.Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> NoteExistAsync(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> NoteExistAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SoftDeleteNoteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateNoteAsync(Models.Note note)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
