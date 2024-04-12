31ae8a7470455b4ab14ae75aeed9f3141f5af6c1
RogerCodes <rogelioherreradev@gmail.com>
2024-04-11 23:01:27 -0600

    Added 'Note' Model under new Directory '/Models'

---
+++ 3LayerAPI/Models/Note.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _3LayerAPI.Models
+{
+    [Table("Notes")]
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(255)]
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+        public bool IsPrivate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
