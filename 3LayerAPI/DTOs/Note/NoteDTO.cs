c6391464b0b2d2362a329d9a5cc2dfefd70355f7
RogerCodes <rogelioherreradev@gmail.com>
2024-04-13 11:01:41 -0600

    Created DTOs (Data Transfer Objects) directory, created 'NoteDTO.cs' inside of same directory

---
+++ 3LayerAPI/DTOs/Note/NoteDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _3LayerAPI.DTOs.Note
+{
+    public class NoteDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(50)]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(255)]
+        [Display(Name = "Content")]
+        public string Content { get; set; }
+        public bool IsPrivate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
