439793b1eba225023fa987dceb8a7afbc8016d37
RogerCodes <rogelioherreradev@gmail.com>
2024-04-14 15:56:04 -0600

    Added 'NoteController'

---
+++ 3LayerAPI/Controllers/NoteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _3LayerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+    }
+}
